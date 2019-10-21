bdd84a8f9d29c9ef967267c450c965538c3da62c
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-10-21 23:53:59 +0000

    Common application_controller.rb application_helper.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,16 @@ class ApplicationController < ActionController::Base
 
   #application_controllerはcontrollerで共通で使用するコードを指定
   # 現在ログインしているuserを返すメソッド
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  # end
 
   # ログインしているか確認するメソッド
-  def logged_in?
-    !current_user.nil?
-  end
+  # def logged_in?
+  #   !current_user.nil?
+  # end
+  
+  # application_controller.rbとapplication_helper.rb内の記述から重複したコードを削除し変更
+  include CommonActions
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 module ApplicationHelper
     # application_helperなどのhelperはviewで使用する共通のコードを指定
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
+    # def current_user
+    #     @current_user ||= User.find_by(id: session[:user_id])
+    # end
     
-    def logged_in?
-        !current_user.nil?
-    end
+    # def logged_in?
+    #     !current_user.nil?
+    # end
+    
+    # application_controller.rbとapplication_helper.rb内の記述から重複したコードを削除し変更
+    include CommonActions
 end
